59031c5cc71b8e2e25a20bde5bc57adad03e9d8a
Zhengtao <56385159+ANKer661@users.noreply.github.com>
2024-07-11 23:09:29 +0800

    feat: add tensor and some operations (#1)
    - Implement `Tensor` class with support for some basic operations (add,
        substraction, multiply, divide, matrix multiply)
    - Implement each operation as a subclass of abstract class `Operation`
    - Include `forward` and `backward` function for auto-grad in operations
    - Add unit tests for Tensor to verify tensor creation, computation and
        diffrentiation
    - Add unit tests for `Operation` to verify `forward` and `backward`
        process

---
+++ src/operations.py
@@ -0,0 +1,132 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+
+import numpy as np
+from numpy.typing import NDArray
+
+if TYPE_CHECKING:
+    from .tensor import Tensor
+
+
+class Operation(ABC):
+    """
+    Abstract base class for all operations on tensors.
+
+    Attributes:
+        inputs: A tuple of input tensors for the operation.
+    """
+
+    inputs: tuple[Tensor, ...]
+
+    def __call__(self, *inputs: Tensor) -> Tensor:
+        """
+        Calls the operation and computes the forward pass.
+
+        Args:
+            inputs: A variable number of Tensor objects to perform the operation on.
+
+        Returns:
+            A new Tensor resulting from the operation.
+        """
+        from .tensor import Tensor  # avoid circular import
+
+        self.inputs = inputs
+        outputs = self.forward(*[t.data for t in inputs])
+        result = Tensor(outputs, require_grad=any(t.require_grad for t in inputs))
+
+        if result.require_grad:
+
+            def backward_fn(input_grad: NDArray) -> None:
+                grads = self.backward(input_grad)
+                for tensor, grad in zip(inputs, grads):
+                    if tensor.require_grad:
+                        tensor.backward(grad)
+
+            result._backward_fn = backward_fn
+
+        return result
+
+    @abstractmethod
+    def forward(self, *args: NDArray) -> NDArray:
+        """
+        Computes the forward pass of the operation.
+
+        Args:
+            args (NDArray): A variable number of NDArray representing
+                the inputs data.
+
+        Returns:
+            NDArray: result of the operation.
+        """
+        pass
+
+    @abstractmethod
+    def backward(self, grad: NDArray) -> tuple[NDArray, ...]:
+        """
+        Computes the backward pass of the operation.
+
+        Args:
+            grad (NdArray): An NDArray of the gradient of the output.
+
+        Returns:
+            tuple[NDArray, ...]: A tuple of NDArrays representing the
+                gradients of the inputs.
+        """
+        pass
+
+
+class Add(Operation):
+    """The element-wise addition of two NDArrays."""
+
+    def forward(self, a: NDArray, b: NDArray) -> NDArray:
+        return a + b
+
+    def backward(self, grad: NDArray) -> tuple[NDArray, NDArray]:
+        return grad, grad
+
+
+class Subtract(Operation):
+    """The element-wise subtraction of two NDArrays."""
+
+    def forward(self, a: NDArray, b: NDArray) -> NDArray:
+        return a - b
+
+    def backward(self, grad: NDArray) -> tuple[NDArray, NDArray]:
+        return grad, -grad
+
+
+class Multiply(Operation):
+    """The element-wise multiplication of two NDArrays."""
+
+    def forward(self, a: NDArray, b: NDArray) -> NDArray:
+        return a * b
+
+    def backward(self, grad: NDArray) -> tuple[NDArray, NDArray]:
+        a, b = self.inputs
+        return grad * b.data, grad * a.data
+
+
+class Divide(Operation):
+    """The element-wise division of two NDArrays, avoiding division by zero."""
+
+    def forward(self, a: NDArray, b: NDArray) -> NDArray:
+        # avoid dividing by 0
+        epsilon = 1e-5
+        safe_b = np.where(b != 0, b, epsilon)
+        return a / safe_b
+
+    def backward(self, grad: NDArray) -> tuple[NDArray, NDArray]:
+        a, b = self.inputs
+        return grad / b.data, -grad * a.data / (b.data**2)
+
+
+class MatrixMultiply(Operation):
+    """The matrix multiplication of two NDArrays."""
+
+    def forward(self, a: NDArray, b: NDArray) -> NDArray:
+        return np.matmul(a, b)
+
+    def backward(self, grad: NDArray) -> tuple[NDArray, NDArray]:
+        a, b = self.inputs
+        return np.matmul(grad, b.data.T), np.matmul(a.data.T, grad)
